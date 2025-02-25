10a5f71bdcee3fa467f567c741e43baafb4fa32e
Muiz-Dev <muiz.dev.io@gmail.com>
2025-02-25 09:11:54 +0100

    update config.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_NAME', 'invoice');
 
-define('JWT_SECRET', 'your_secret_key');
+define('JWT_SECRET', 'da2b760e930bf0145078c8475d73b3004108363dbe7ed2d97c5d6c0be6743ead');
 
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
